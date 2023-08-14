a5a83b17d8f71e5be77d7f1aa20c6b385cb22bec
zahra <me@gmail.com>
2023-08-14 09:45:24 -0700

    alphabets without e or q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program to print lowerCase Alphabets without e and q
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		if (l == 'e' || l == 'q')
+		{
+			continue;
+			l++;
+		}
+		else
+		{
+			putchar(l);
+			l++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
