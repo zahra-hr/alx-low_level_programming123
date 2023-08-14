4d662100c2efddabd3b14bc32c4448ebb2d8ab1d
zahra <me@gmail.com>
2023-08-13 20:40:36 -0700

    alphab

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program to display Alphabet
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	for (char i = 'a'; i < 'z'; i++)
+		putchar("%c", i);
+	return (0);
+}
