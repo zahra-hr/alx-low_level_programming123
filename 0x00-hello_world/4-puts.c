41f32a64be3bd9ddd88e60094919895bbfe74312
zahra <me@gmail.com>
2023-08-14 17:50:45 -0700

    string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program to print string
+ * Return: 0 (succes)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
