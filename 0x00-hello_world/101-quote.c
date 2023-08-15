7a9d558528585db92645eb28be8176828ead61ee
zahra <me@gmail.com>
2023-08-14 18:41:07 -0700

    hard

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program to print numbers
+ * Return: 0 (succes)
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
