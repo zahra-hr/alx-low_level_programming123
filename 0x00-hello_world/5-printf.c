c2ceddd3532ef9aaf9381935929a904278b04b8d
zahra <me@gmail.com>
2023-08-14 17:58:47 -0700

    string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A C program to print string
+ * Return: 0 (succes)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
