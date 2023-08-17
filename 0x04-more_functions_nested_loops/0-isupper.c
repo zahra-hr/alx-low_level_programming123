8ef8a1c3ad3591d22b632cdb10d5e064031e21bb
zahra <me@gmail.com>
2023-08-17 16:54:15 -0700

    new funct

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check if letter is uppercase
+ *
+ * @c: parameter
+ *
+ * Return: 1 success 0 failed
+*/
+
+int _isupper(int c)
+{
+	if (c <= 'Z' || c >= 'A')
+		return (1);
+	else
+		return (0);
+}
