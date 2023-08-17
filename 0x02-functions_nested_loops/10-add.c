fdca82535f765868f6e4a045bfe8810e31e7672b
zahra <me@gmail.com>
2023-08-16 19:20:56 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -  function that adds two integers
+ * @x: first parameter
+ * @y: second parameter
+ * Return: the sum x + y
+*/
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
