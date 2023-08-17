fae812177f019d115740f755602448dcaf45e324
zahra <me@gmail.com>
2023-08-16 18:36:55 -0700

    clock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int i = 0;
+	int j;
+
+	while (i <= 23)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			if (i < 10)
+				_putchar('0' + 0);
+			_putchar('0' + i);
+			_putchar(':');
+			if (j < 10)
+				_putchar('0' + 0);
+			_putchar('0' + j);
+			_putchar('\n');
+}
