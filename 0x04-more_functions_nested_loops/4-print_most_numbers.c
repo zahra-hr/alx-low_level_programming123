ca09e97fb4b1591cd98447df709212726f073a57
zahra <me@gmail.com>
2023-08-17 19:10:24 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * print_most_numbers - function to print digits
+*/
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if (i == 50 || i == 52)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i);
+		i++;
+	}
+}
