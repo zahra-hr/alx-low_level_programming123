603e76111bc0929c63f12e992f28864972e3b84e
zahra <me@gmail.com>
2023-08-17 19:02:09 -0700

    nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - utilizes putchar to print numbers
+*/
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
