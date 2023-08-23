7a6e248adbe41cb94e421cc0abfa24fda5f68ccc
zahra <me@gmail.com>
2023-08-22 19:15:57 -0700

    pass

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that generates random valid passwords
+ * Return: 0
+*/
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s = s + c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+	return (0);
+}
