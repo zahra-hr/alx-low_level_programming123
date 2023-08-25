deb0855664ad1953852ea540d42c57f457a4d9e1
zahra <me@gmail.com>
2023-08-24 19:10:04 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * flip - function to change string
+ * @alpha1: parameter
+ * @alpha2: parameter
+ * @s: parameter
+ * Return: character
+*/
+
+char flip(char *alpha1, char *alpha2, s)
+{
+	int i = 0;
+
+	while (alpha1[i] != '\0')
+	{
+		if (s == alpha1[i])
+			s = alpha2[i];
+		i++;
+	}
+	return (s);
+}
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @str: parameter
+ * Return: string
+*/
+
+char *rot13(char *str)
+{
+	char *p = str;
+	char a1[] = "ABCDEFGHIJKLM";
+	char a2[] = "NOPQRSTUVWXYZ";
+	char a3[] = "abcdefghijklm";
+	char a4[] = "nopqrstuvwxyz";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		flip(a1, a2, p[i]);
+		flip(a2, a1, p[i]);
+		flip(a3, a4, p[i]);
+		flip(a4, a3, p[i]);
+		i++;
+	}
+	return (p);
+}
