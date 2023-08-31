97e9784bf7324fbe0be4d8530b69573c305d93e5
zahra <me@gmail.com>
2023-08-30 18:08:02 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,6 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('');
-	else
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
